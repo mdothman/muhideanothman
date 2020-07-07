c80bde4a686fc6ace1a9603fae49fd91c81b5ca7
BEELZ <othmanmuhidean@gmail.com>
2020-07-06 17:28:14 -0700

    Selected a component for my header

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,57 @@
-import React from "react";
-import { useStoreContext } from "../../utils/GlobalState";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+import EmailIcon from '@material-ui/icons/Email';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-function Nav() {
-  const [store] = useStoreContext();
+const useStyles = makeStyles((theme) => ({
+  emailIcon:{
+    position:"static"
+  },
+  linkedInIcon:{
+    position:"static",
+    // marginRight: theme.spacing(2),
+  },
+  gitHubIcon:{
+    position:"static",
+    // marginRight: theme.spacing(2),
+  },
+  // menuButton: {
+    // position: "static",
+    // marginRight: theme.spacing(.5),
+  // },
+}));
+
+export default function Nav() {
+  const classes = useStyles();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        Home
-      </a>
-      <a className="navbar-brand" href="/resume">
-        Resume
-      </a>
-      <a className="navbar-brand" href="/projects">
-        Projects
-      </a>
-      <a className="navbar-brand" href="/contact">
-        Contact Me
-      </a>
-    
-      {store.loading ? <a className="navbar-brand ml-auto" href="/">Loading...</a> : <></>}
-    </nav>
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar variant="dense">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+            
+          </IconButton> */}
+          <IconButton edge="start" className={classes.gitHubIcon} color="inherit" aria-label="menu">
+            <GitHubIcon />
+            
+          </IconButton>
+          <IconButton edge="start" className={classes.linkedInIcon} color="inherit" aria-label="menu">
+            <LinkedInIcon />
+            
+          </IconButton>
+          <IconButton edge="start" className={classes.gitHubIcon} color="inherit" aria-label="menu">
+            <EmailIcon />
+            
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
-
-export default Nav;
