1b2a1035b02280740313746d6de92dd20810b7a4
BEELZ <othmanmuhidean@gmail.com>
2020-07-06 20:30:02 -0700

    Progress made, will most likely stick to this design for home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import Resume from "./pages/Resume";
-import Portfolio from "./pages/Portfolio";
 import ContactInfo from "./pages/ContactInfo";
 import StickyFooter from "./components/StickyFooter"
 import Nav from "./components/Nav";
@@ -30,8 +28,6 @@ function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/home" component={Home} />
-              <Route exact path="/resume" component={Resume} />
-              <Route exact path="/projects" component={Portfolio} />
               <Route exact path="/contact" component={ContactInfo} />
               <Route component={NoMatch} />
             </Switch>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Portfolio from '../components/Portfolio'
 import { makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
   main: {
     marginTop: theme.spacing(8),
     marginBottom: theme.spacing(2),
@@ -20,7 +25,8 @@ const useStyles = makeStyles((theme) => ({
 const Home = () => {
   const classes = useStyles();
   return (
-    <Container component="main" className={classes.main} maxWidth="sm">
+  <main> 
+    <div className="main"><Container component="main" className={classes.main} maxWidth="sm">
         <Typography variant="h2" component="h1" gutterBottom>
           Muhi-Dean Othman
         </Typography>
@@ -29,6 +35,12 @@ const Home = () => {
         </Typography>
         <Typography variant="body1">"The best way to predict the future is, to create it." —Abraham Lincoln</Typography>
       </Container>
+      </div>
+    <Container className={classes.cardGrid} maxWidth="md">
+        <Portfolio />
+      </Container></main>
+   
+      
 
 
 
